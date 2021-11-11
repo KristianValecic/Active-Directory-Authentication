31f3e8f6eca5e08beef3c2463169c656e722bb8e
KristianValecic <kristian.valecic@gmail.com>
2021-11-11 10:35:25 +0100

    Add project files.

---
+++ Login/Login.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Login.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+            <asp:Label id="lblName" runat="server"  Text="Korisnicko ime" Width="92px"></asp:Label> 
+         
+            <input id="Username" type="text" runat="server"/>
+           
+            <br />
+
+            <asp:Label id="lblPass" runat="server"  Text="Lozinka" Width="50px"></asp:Label>
+            <input id="Password" type="password"  runat="server"/> 
+
+            <br /><br />
+
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+
+            <br />
+
+            <asp:Label id="ErrorMessage" runat="server">Krivo upisani podaci</asp:Label>
+           
+            <br />
+
+            <asp:ListBox ID="txtResults" runat="server"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ Login/Login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace Login
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ErrorMessage.Visible = false;
+            txtResults.Visible = false;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (GetAllUsers())
+            {
+                txtResults.Visible = true;
+            }
+            else
+            {
+                ErrorMessage.Visible = true;
+            }
+        }
+
+        protected bool GetAllUsers()
+        {
+
+            DirectoryEntry de = new DirectoryEntry("LDAP://192.168.229.130", Username.Value, Password.Value);
+
+            DirectorySearcher ds = new DirectorySearcher(de);
+            ds.Filter = "(&(objectCategory=User)(objectClass=person))";
+
+            SearchResultCollection results;
+            try
+            {
+                txtResults.Items.Clear();
+                results = ds.FindAll();
+                foreach (SearchResult sr in results)
+                {
+                    txtResults.Visible = true;
+                    txtResults.Items.Add(sr.Properties["name"][0].ToString());
+                }
+                return true;
+            }
+            catch
+            {
+                txtResults.Items.Clear();
+                return false;
+            }
+
+        }
+
+    }
+}
